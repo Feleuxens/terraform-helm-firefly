9855a9fbb1034ba9ef77e1c59c5a60c4f8fa6373
Feleuxens <felixgrueb@icloud.com>
2023-12-07 00:38:25 +0100

    Move module

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "helm_release" "firefly-db" {
+  name = "firefly-db"
+
+  repository       = "https://firefly-iii.github.io/kubernetes"
+  chart            = "firefly-db"
+  create_namespace = true
+  namespace        = "firefly"
+
+  values = [
+    templatefile("${path.module}/helm-values/firefly-db.yaml", {
+    })
+  ]
+}
+
+resource "helm_release" "firefly" {
+  name = "firefly"
+
+  repository       = "https://firefly-iii.github.io/kubernetes"
+  chart            = "firefly-iii"
+  create_namespace = true
+  namespace        = "firefly"
+
+  values = [
+    templatefile("${path.module}/helm-values/firefly.yaml", {
+      domain : var.domain,
+      cpu_request : var.cpu_request,
+      memory_request : var.memory_request,
+      memory_limit : var.memory_limit
+    })
+  ]
+  set_sensitive {
+    name  = "secrets.env.APP_KEY"
+    value = var.firefly_app_key
+  }
+  set_sensitive {
+    name = "cronjob.auth.token"
+    value = random_password.token.result
+  }
+
+  depends_on = [kubernetes_namespace.firefly-db]
+}
+
+resource "random_password" "token" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "domain" {
+  description = "Domain name"
+  type        = string
+}
+
+variable "firefly_app_key" {
+  description = "Firefly App Key"
+  type = string
+}
+
+variable "cpu_request" {
+  type = string
+  default = "10m"
+}
+
+variable "memory_request" {
+  type = string
+  default = "256Mi"
+}
+
+variable "memory_limit" {
+  type = string
+  default = "512Mi"
+}
